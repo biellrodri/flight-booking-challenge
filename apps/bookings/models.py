d511c17343edf9e1b86f8fcacd08798d8bddf32f
Gabriel Rodrigues <gabrielsilva.8850@gmail.com>
2026-03-18 08:57:03 -0300

    feat: implement core domain models (planes, flights, customers, bookings)

--- apps/bookings/models.py
+++ apps/bookings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Booking(models.Model):
+    flight = models.ForeignKey(
+        "flights.Flight", on_delete=models.CASCADE, related_name="bookings"
+    )
+    customer = models.ForeignKey(
+        "customer.Customer", on_delete=models.CASCADE, related_name="bookings"
+    )
+
+    seat_number = models.PositiveIntegerField()
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.customer} - {self.flight}"
--- apps/flights/models.py
+++ apps/flights/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Flight(models.Model):
+    origin = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    departure_time = models.DateField()
+
+    plane = models.ForeignKey(
+        "planes.Plane", on_delete=models.CASCADE, related_name="flights"
+    )
+
+    def __str__(self):
+        return f"{self.origin} -> {self.destination}"
