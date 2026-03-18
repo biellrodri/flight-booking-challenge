3b0e58e64f51cae3fe9fecc44b381a9495f2f0ce
Gabriel Rodrigues <gabrielsilva.8850@gmail.com>
2026-03-18 13:57:30 -0300

    feat: addbooking validation rules

--- apps/bookings/models.py
+++ apps/bookings/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -12,5 +13,26 @@ class Booking(models.Model):
     seat_number = models.PositiveIntegerField()
     created_at = models.DateField(auto_now_add=True)
 
+    def clean(self):
+        total_seats = self.flight.plane.total_seats
+        booked_seats = self.flight.bookings.count()
+
+        if booked_seats >= total_seats:
+            raise ValidationError("Todos os assentos ocupados para este voo.")
+
+        if self.seat_number > total_seats:
+            raise ValidationError("Número de assento inválido")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.customer} - {self.flight}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["flight", "seat_number"], name="unique_seat_per_flight"
+            )
+        ]
