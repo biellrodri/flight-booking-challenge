ac41b165484750740fdac7005af7245ebf747dcc
Gabriel Rodrigues <gabrielsilva.8850@gmail.com>
2026-03-24 23:36:39 -0300

    feat: handle model validation errors properly in booking serializer

--- apps/bookings/serializers.py
+++ apps/bookings/serializers.py
@@ -1,9 +1,27 @@
+from django.core.exceptions import ValidationError as DjangoValidationError
 from rest_framework import serializers
 
 from .models import Booking
 
 
 class BookingSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Booking model.
+    """
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except DjangoValidationError as e:
+            raise serializers.ValidationError(e.message_dict)
+
+    def update(self, instance, validated_data):
+        try:
+            return super().update(instance, validated_data)
+        except DjangoValidationError as e:
+            raise serializers.ValidationError(e.message_dict)
+
     class Meta:
         model = Booking
         fields = "__all__"
+        validators = []
