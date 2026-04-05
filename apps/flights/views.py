e26993dce23ba69ceebe8182c8cbc5534aaa0cae
Gabriel Rodrigues <gabrielsilva.8850@gmail.com>
2026-04-05 20:45:51 -0300

    feat: add flights.html page

--- apps/flights/views.py
+++ apps/flights/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from rest_framework import viewsets
 
 from .models import Flight
@@ -7,3 +8,9 @@ from .serializers import FlightSerializer
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+
+
+def flights_list(request):
+    flights = Flight.objects.all().order_by("departure_time")
+
+    return render(request, "pages/flights.html", {"flights": flights})
--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 from apps.bookings.views import BookingViewSet
 from apps.customers.views import CustomerViewSet
-from apps.flights.views import FlightViewSet
+from apps.flights.views import FlightViewSet, flights_list
 from apps.planes.views import PlaneViewSet
 
 router = DefaultRouter()
@@ -14,6 +14,7 @@ router.register(r"planes", PlaneViewSet)
 router.register(r"flights", FlightViewSet)
 
 urlpatterns = [
+    path("", flights_list, name="flights_list"),
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls")),
